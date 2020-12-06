60382fae8fa504788644d50f53e902ba59827890
plapla-1357 <73687455+plapla-1357@users.noreply.github.com>
2020-12-06 18:09:00 +0100

    upate js html et css
    le js a ameloirer

--- clicker.js
+++ clicker.js
@@ -1,1 +1,29 @@
+var x = 0;
+var furnace = 0;
 
+function add() {
+	var id = document.getElementById("canele");
+	x = id.innerHTML;
+	x++;
+	id.innerHTML = x;
+	
+	if (x >= 25) {
+		document.getElementById("acheter_four").removeAttribute('disabled');
+	} else {
+		document.getElementById("acheter_four").disabled = true;
+	}
+};
+
+function buy_furnace(){
+	if (x >= 25 ){
+		x-= 25;
+		furnace += 1 ;
+	}
+	
+};
+
+
+
+
+
+		
